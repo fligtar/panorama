78a8df5aac1f81d919a3e2d64e4c2c056067cda3
Justin Scott <fligtar@gmail.com>
2011-02-21 22:54:27 -0800

    Allow for updating existing rows with new columns

--- lib/backfill.php
+++ lib/backfill.php
@@ -1,14 +1,14 @@
 <?php
 error_reporting(E_ERROR);
 define('OVERLORD', true);
-require_once '../reports/contributions/summary.php';
+require_once '../reports/addons/creation.php';
 
 ob_start();
 
-$report = new ContributionSummary;
+$report = new AddonCreation;
 
 //$report->backfill('2011-01-14');
-$report->backfill('2011-01-02', '2011-01-15');
+$report->backfill('2010-05-07', '2010-05-07');
 
 ob_end_flush();
 
