9e7c862a415b6e62e45b70db76654e445ec24438
Justin Scott <fligtar@gmail.com>
2011-05-11 14:37:25 -0700

    hive jar no longer needed

--- heavylifting/daily.sh
+++ heavylifting/daily.sh
@@ -13,44 +13,44 @@ DATA=/home/fligtar/data/$DATE
 mkdir $DATA
 
 # DEBUG
-#hive --auxpath '/usr/lib/hive/lib/hive_contrib.jar' -e "SHOW PARTITIONS research_logs;"
+#hive -e "SHOW PARTITIONS research_logs;"
 
 # Discovery Pane views
-hive --auxpath '/usr/lib/hive/lib/hive_contrib.jar' -e "SELECT COUNT(1) FROM research_logs WHERE ds = '$DATE' AND domain='services.addons.mozilla.org' AND request_url LIKE '%discovery/pane%';" | tee $DATA/discovery.txt
+hive -e "SELECT COUNT(1) FROM research_logs WHERE ds = '$DATE' AND domain='services.addons.mozilla.org' AND request_url LIKE '%discovery/pane%';" | tee $DATA/discovery.txt
 
 # Discovery Pane details
-hive --auxpath '/usr/lib/hive/lib/hive_contrib.jar' -e "SELECT COUNT(1) FROM research_logs WHERE ds = '$DATE' AND domain='services.addons.mozilla.org' AND request_url LIKE '%discovery/addon%';" | tee $DATA/discovery-details.txt
+hive -e "SELECT COUNT(1) FROM research_logs WHERE ds = '$DATE' AND domain='services.addons.mozilla.org' AND request_url LIKE '%discovery/addon%';" | tee $DATA/discovery-details.txt
 
 # RETIRED by CDN
 # ABP Icon views by referrer
-#hive --auxpath '/usr/lib/hive/lib/hive_contrib.jar' -e "SELECT empty_string_1, COUNT(1) as num FROM research_logs WHERE ds = '$DATE' AND (domain='addons.mozilla.org' OR domain='static.addons.mozilla.org') AND request_url LIKE '%/images/addon_icon/1865/%' GROUP BY empty_string_1 ORDER BY num DESC;" > $DATA/abp-icon-referrers.txt
+#hive -e "SELECT empty_string_1, COUNT(1) as num FROM research_logs WHERE ds = '$DATE' AND (domain='addons.mozilla.org' OR domain='static.addons.mozilla.org') AND request_url LIKE '%/images/addon_icon/1865/%' GROUP BY empty_string_1 ORDER BY num DESC;" > $DATA/abp-icon-referrers.txt
 
 # RETIRED by CDN
 # ABP Icon views total
-#hive --auxpath '/usr/lib/hive/lib/hive_contrib.jar' -e "SELECT COUNT(1) FROM research_logs WHERE ds = '$DATE' AND (domain='addons.mozilla.org' OR domain='static.addons.mozilla.org') AND request_url LIKE '%/images/addon_icon/1865/%';" | tee $DATA/abp-icon-total.txt
+#hive -e "SELECT COUNT(1) FROM research_logs WHERE ds = '$DATE' AND (domain='addons.mozilla.org' OR domain='static.addons.mozilla.org') AND request_url LIKE '%/images/addon_icon/1865/%';" | tee $DATA/abp-icon-total.txt
 
 # API Total
-hive --auxpath '/usr/lib/hive/lib/hive_contrib.jar' -e "SELECT COUNT(1) FROM research_logs WHERE ds = '$DATE' AND domain='services.addons.mozilla.org' AND request_url LIKE '%api%';" | tee $DATA/api-total.txt
+hive -e "SELECT COUNT(1) FROM research_logs WHERE ds = '$DATE' AND domain='services.addons.mozilla.org' AND request_url LIKE '%api%';" | tee $DATA/api-total.txt
 
 # API Featured
-hive --auxpath '/usr/lib/hive/lib/hive_contrib.jar' -e "SELECT COUNT(1) FROM research_logs WHERE ds = '$DATE' AND domain='services.addons.mozilla.org' AND request_url LIKE '%api/%/list/featured/%';" | tee $DATA/api-featured.txt
+hive -e "SELECT COUNT(1) FROM research_logs WHERE ds = '$DATE' AND domain='services.addons.mozilla.org' AND request_url LIKE '%api/%/list/featured/%';" | tee $DATA/api-featured.txt
 
 # API Search
-hive --auxpath '/usr/lib/hive/lib/hive_contrib.jar' -e "SELECT COUNT(1) FROM research_logs WHERE ds = '$DATE' AND domain='services.addons.mozilla.org' AND request_url LIKE '%api/%/search/%';" | tee $DATA/api-search.txt
+hive -e "SELECT COUNT(1) FROM research_logs WHERE ds = '$DATE' AND domain='services.addons.mozilla.org' AND request_url LIKE '%api/%/search/%';" | tee $DATA/api-search.txt
 
 # API GUID Search
-hive --auxpath '/usr/lib/hive/lib/hive_contrib.jar' -e "SELECT COUNT(1) FROM research_logs WHERE ds = '$DATE' AND domain='services.addons.mozilla.org' AND request_url LIKE '%api/%/search/guid%';" | tee $DATA/api-guid-search.txt
+hive -e "SELECT COUNT(1) FROM research_logs WHERE ds = '$DATE' AND domain='services.addons.mozilla.org' AND request_url LIKE '%api/%/search/guid%';" | tee $DATA/api-guid-search.txt
 
 # API Add-on
-hive --auxpath '/usr/lib/hive/lib/hive_contrib.jar' -e "SELECT COUNT(1) FROM research_logs WHERE ds = '$DATE' AND domain='services.addons.mozilla.org' AND request_url LIKE '%api/%/addon/%';" | tee $DATA/api-addon.txt
+hive -e "SELECT COUNT(1) FROM research_logs WHERE ds = '$DATE' AND domain='services.addons.mozilla.org' AND request_url LIKE '%api/%/addon/%';" | tee $DATA/api-addon.txt
 
 # Number of add-ons installed (Firefox)
-#hive --auxpath '/usr/lib/hive/lib/hive_contrib.jar' -e "SELECT commas, COUNT(1) FROM (SELECT (LENGTH(request_url) - LENGTH(REGEXP_REPLACE(request_url, ',', '')) + 1) as commas FROM research_logs WHERE ds = '$DATE' AND domain='services.addons.mozilla.org' AND request_url LIKE '%api/%/search/guid%972ce4c6-7e08-4474-a285-3208198ce6fd%src=firefox%') temp GROUP BY commas ORDER BY commas;" | tee $DATA/metadata-installed-fx.txt
+#hive -e "SELECT commas, COUNT(1) FROM (SELECT (LENGTH(request_url) - LENGTH(REGEXP_REPLACE(request_url, ',', '')) + 1) as commas FROM research_logs WHERE ds = '$DATE' AND domain='services.addons.mozilla.org' AND request_url LIKE '%api/%/search/guid%972ce4c6-7e08-4474-a285-3208198ce6fd%src=firefox%') temp GROUP BY commas ORDER BY commas;" | tee $DATA/metadata-installed-fx.txt
 
 # Number of add-ons installed (Mobile)
-#hive --auxpath '/usr/lib/hive/lib/hive_contrib.jar' -e "SELECT commas, COUNT(1) FROM (SELECT (LENGTH(request_url) - LENGTH(REGEXP_REPLACE(request_url, ',', '')) + 1) as commas FROM research_logs WHERE ds = '$DATE' AND domain='services.addons.mozilla.org' AND request_url LIKE '%api/%/search/guid%972ce4c6-7e08-4474-a285-3208198ce6fd%src=mobile%') temp GROUP BY commas ORDER BY commas;" | tee $DATA/metadata-installed-mobile.txt
+#hive -e "SELECT commas, COUNT(1) FROM (SELECT (LENGTH(request_url) - LENGTH(REGEXP_REPLACE(request_url, ',', '')) + 1) as commas FROM research_logs WHERE ds = '$DATE' AND domain='services.addons.mozilla.org' AND request_url LIKE '%api/%/search/guid%972ce4c6-7e08-4474-a285-3208198ce6fd%src=mobile%') temp GROUP BY commas ORDER BY commas;" | tee $DATA/metadata-installed-mobile.txt
 
 # Firefox start-up performance
-#hive --auxpath '/usr/lib/hive/lib/hive_contrib.jar' -e "SELECT guid, src, appos, appversion, tmain, tfirstpaint, tsessionrestored FROM addons_pings WHERE ds = '$DATE';" > $DATA/metadata-perf.txt
+#hive -e "SELECT guid, src, appos, appversion, tmain, tfirstpaint, tsessionrestored FROM addons_pings WHERE ds = '$DATE';" > $DATA/metadata-perf.txt
 
 scp -r $DATA/ fligtar@khan:./panorama/hadoop-drop/
--- heavylifting/hive.py
+++ heavylifting/hive.py
@@ -6,7 +6,8 @@ def query(query):
     """Performs a HIVE query and writes it to a file, returning the file path."""
     
     file_path = FILES + '/' + str(time.time()) + '.txt'
-    os.system("""hive --auxpath '/usr/lib/hive/lib/hive_contrib.jar' -e "%s" -S > %s""" % (query, file_path))
+    #os.system("""hive --auxpath '/usr/lib/hive/lib/hive_contrib.jar' -e "%s" -S > %s""" % (query, file_path))
+    os.system("""hive -e "%s" -S > %s""" % (query, file_path))
     
     return file_path
 
